da95a5433629b33f2783915ce74ab5b3af917619
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-04-05 19:00:12 -0700

    Removes web2py files
    We don't need these

--- frontend/routes.py
+++
@@ -1,7 +0,0 @@
-# makes the PopperCI application the default application inside web2py
-# (e.g. no need to type 127.0.0.1:8000/popperci/default/index)
-routers = dict(
-    BASE=dict(
-        default_application='popperci',
-    )
-)
