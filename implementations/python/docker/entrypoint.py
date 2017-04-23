e7b24305508a96d31170f6766bb93ed46c3fb90c
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-04-23 18:39:53 +0200

    Streams stdout to file and prints progress as dots
    Also, adds ability to timeout pipeline executions.
    
    fixes systemslab/popper#83
    fixes systemslab/popper#108

--- implementations/python/docker/entrypoint.py
+++ implementations/python/docker/entrypoint.py
@@ -1,37 +1,53 @@
 #!/usr/bin/env python
 
-from subprocess import Popen, PIPE
-from os import path
 import argparse
+import os
+import subprocess
+import time
+import signal
+import sys
+
+from subprocess import check_output
+from os import path
 
 stages = ['setup.sh', 'run.sh', 'teardown.sh', 'validate.sh']
 
 
-def sh(cmd):
-    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-    out, err = process.communicate()
-    ecode = process.returncode
+def execute(stage, timeout):
+    time_limit = time.time() + timeout
 
-    return ecode, out, err
+    out_fname = 'popper_logs/{}.{}'.format(stage, 'out')
+    err_fname = 'popper_logs/{}.{}'.format(stage, 'err')
+
+    sys.stdout.write('Running stage: ' + stage + ' ')
+
+    with open(out_fname, "wb") as outf, open(err_fname, "wb") as errf:
+        p = subprocess.Popen('./' + stage, shell=True, stdout=outf, stderr=errf,
+                             preexec_fn=os.setsid)
+
+        while p.poll() is None:
+            sys.stdout.write('.')
+
+            if time.time() > time_limit:
+                os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+                sys.stdout.write(' time out!')
+                break
+
+            time.sleep(20)
+
+    sys.stdout.write('\n')
+
+    return p.poll()
 
 
-def writefile(fname, content):
-    with open(fname, 'w') as f:
+def append_to_file(fname, content):
+    with open(fname, 'aw') as f:
         f.write(content)
 
 
-def execute(stage):
-    ecode, out, err = sh(stage)
-
-    writefile('popper_logs/{}.{}'.format(stage, 'out'), out)
-    writefile('popper_logs/{}.{}'.format(stage, 'err'), err)
-
-    return ecode, out
-
-
-def check_experiment():
-    sh('rm -rf popper_logs/')
-    sh('mkdir -p popper_logs/')
+def check_experiment(skip, timeout):
+    check_output('rm -rf popper_logs/ popper_status', shell=True)
+    check_output('mkdir -p popper_logs/', shell=True)
 
     STATUS = "SUCCESS"
 
@@ -40,12 +56,10 @@ def check_experiment():
         if not path.isfile(stage):
             continue
 
-        if stage in args.skip.split(','):
+        if skip and stage in skip.split(','):
             continue
 
-        print("Running stage: " + stage)
-
-        ecode, out = execute('./' + stage)
+        ecode = execute(stage, timeout)
 
         if ecode != 0:
             print("Stage {} failed. Check logs for details.".format(stage))
@@ -53,17 +67,36 @@ def check_experiment():
             break
 
         if stage == 'validate.sh':
-            for line in out.splitlines():
-                if '[true]' not in line:
-                    break
             STATUS = "GOLD"
+            with open('popper_logs/validate.sh.out', 'r') as f:
+                validate_output = f.readlines()
+                if len(validate_output) == 0:
+                    STATUS = "SUCCESS"
+                for line in validate_output:
+                    if '[true]' not in line:
+                        STATUS = "SUCCESS"
 
-    writefile('popper_status', STATUS + '\n')
+    append_to_file('popper_status', STATUS + '\n')
+
+
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--skip', default='',
+    parser.add_argument('--timeout', default=10800, help='Timeout in seconds.')
+    parser.add_argument('--skip', default=None, required=False,
                         help='Comma-separated list of stages to skip.')
     args = parser.parse_args()
-    check_experiment()
+    sys.stdout = Unbuffered(sys.stdout)
+
+    check_experiment(args.skip, args.timeout)
